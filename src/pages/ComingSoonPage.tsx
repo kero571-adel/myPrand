7e942733e1ab8d6c10f29dd11e12828971684c6d
keroles adel <kerolesadel297@gmail.com>
2026-05-30 00:17:33 +0300

    add time in comingsoonPage

--- src/pages/ComingSoonPage.tsx
+++ src/pages/ComingSoonPage.tsx
@@ -16,6 +16,9 @@ export default function ComingSoonPage() {
         <h1 className="text-2xl md:text-4xl font-bold mb-8 animate-pulse">
           Welcome to the Developers is World
         </h1>
+        <h1 className="text-2xl md:text-4xl font-bold mb-8 animate-pulse">
+          2026/5/31
+        </h1>
         <p className="text-3xl md:text-4xl font-semibold text-[#7fb47f] animate-bounce">
           Soon{dots}
         </p>
